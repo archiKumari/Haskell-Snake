3ed7bad80f4d6896670d6f0fbdc0afe2b3005671
archikapiswe <archikumari97@gmail.com>
2020-08-05 17:32:48 +0530

    Added Start Game and Game Over widgets

--- src/UI.hs
+++ src/UI.hs
@@ -15,20 +15,28 @@ import Lens.Micro   ((^.), (&), (.~), (%~))
 
 import Types
 
-
 renderGame :: GameState -> Widget ()
 renderGame gs = case gs ^. gsGameStatusL of
-  GameOver -> drawGameOver gs
+  Initial  -> drawStartGame gs
   Playing  -> drawGameObject gs
-  -- Initial  -> drawInitialWidget
+  Paused -> drawGameObject gs
+  GameOver -> drawGameOver gs
 
-drawGameOver :: GameState ->  Widget ()
-drawGameOver gs = center $ Widget Fixed Fixed $ do
+drawStartGame :: GameState -> Widget ()
+drawStartGame gs = center $ Widget Fixed Fixed $ do
   return $ emptyResult &
-    imageL .~ gameOverImg
-      where
-        gameOverImg = vertCat $
-          (horizCat . fmap makeColorCellImg ) <$> gameOverStr 
+    imageL .~ initImg
+  where initImg = vertCat [border,nameImg,border,optionsImg,border]
+        border = charFill bgColor ' ' 40 1
+        nameImg = string bgColor "         ***** HSnake Game *****        "
+        optionsImg = vertCat [startImg,border,modeImg]
+        startImg = horizCat [charFill bgColor ' ' 2 1,sKey,string bgColor " Start Game                        "]
+        sKey = string (getAttr "key") " S "
+        modeImg = horizCat [charFill bgColor ' ' 2 1,mKey,string bgColor " Select Mode                       "]
+        mKey = string (getAttr "key") " M "
+        lightC = charFill (getAttr "lightGreen") ' ' 2 1
+        darkC = charFill (getAttr "darkGreen") ' ' 2 1  
+        bgColor = getAttr "bgLightGreen"
 
 drawGameObject :: GameState -> Widget ()
 drawGameObject gs = center $ Widget Fixed Fixed $ do
@@ -40,20 +48,18 @@ mkGameImg :: GameState -> Image
 mkGameImg gs = gameImg
       where gameImg = mkHorizBorder.vertCat $ fmap (mkRowImg gs) rowMap
             rowMap = mkRowMap (gsSize gs)
-            bgColor2 = V.black `on` V.rgbColor 50 135 168
-            foodPos = show (gs ^. gsFoodPosL)
 
 mkHorizBorder :: Image -> Image
 mkHorizBorder img = vertCat [border,img,border]
   where border = charFill brColor ' ' width 1
         width = imageWidth img
-        brColor = V.black `on` rgbColor 13 168 10
+        brColor = getAttr "bgDarkGreen"
 
 mkRowImg :: GameState -> [Cordinate] -> Image
 mkRowImg gs cords = horizCat [border,rowImg,border]
   where rowImg = horizCat $ fmap (mkCellImg gs) cords
         border = charFill brColor ' ' 2 1
-        brColor = V.black `on` rgbColor 13 168 10
+        brColor = getAttr "bgDarkGreen"
 
 mkCellImg :: GameState -> Cordinate -> Image
 mkCellImg gs cord
@@ -64,8 +70,8 @@ mkCellImg gs cord
   | even (xCord cord) && even (yCord cord) = charFill bgColor1 ' ' 2 1
   | otherwise = charFill bgColor2 ' ' 2 1
   where 
-    bgColor1 = V.black `on` V.rgbColor 92 230 90
-    bgColor2 = V.black `on` V.rgbColor 55 209 52 
+    bgColor1 = getAttr "lightGreen" 
+    bgColor2 = getAttr "darkGreen"  
 
 mkRowMap :: GameSize -> [[Cordinate]]
 mkRowMap (row,col) | row < 0 || col < 0 = []
@@ -92,42 +98,35 @@ mkScoreImg gs = scoreImg
         brColor = V.black `on` rgbColor 13 168 10
         lColor = flip V.withStyle V.bold $ V.red `on` rgbColor 13 168 10
         
+drawGameOver :: GameState ->  Widget ()
+drawGameOver gs = center $ Widget Fixed Fixed $ do
+  return $ emptyResult &
+    imageL .~ gameOverImg
+    where
+      gameOverImg = vertCat [border,gameOver,border,gameDataImg,border,msgImg,border]
+      border = charFill bgColor ' ' 40 1
+      gameOver = string bgColor "           *****GAME OVER*****          "
+      gameDataImg = horizCat [foodImg,spaceCount,scoreImg]
+      spaceCount = charFill bgColor ' ' (imageWidth border - imageWidth foodImg - imageWidth scoreImg - 2) 1
+      foodImg = horizCat [charFill bgColor ' ' 2 1,charFill bgColor '🍉' 1 1,string bgColor (" " ++ show foodCount)]
+      scoreImg = horizCat [charFill bgColor '🌟' 1 1,string bgColor (" " ++ show score),charFill bgColor ' ' 2 1]
+      foodCount = gs ^. gsFoodCountL
+      score = gs ^. gsScoreL
+      msgImg = vertCat [newGameMsg,border,quitMsg]
+      newGameMsg = horizCat [charFill bgColor ' ' 2 1,enterKeyImg,string bgColor "  Play Again!                  "]
+      quitMsg = horizCat [charFill bgColor ' ' 2 1,quitKeyImg,string bgColor "  Quit The Game                    "]
+      enterKeyImg = string (getAttr "key") " Enter "
+      quitKeyImg = string (getAttr "key") " Q "
+      bgColor = getAttr "bgLightGreen"
+        
+getAttr :: String ->  Attr
+getAttr "darkGreen" = V.black `on` V.rgbColor 55 209 52
+getAttr "lightGreen" = V.black `on` V.rgbColor 92 230 90
+getAttr "heart" = flip V.withStyle V.bold $ V.red `on` rgbColor 13 168 10
+getAttr "bgDarkGreen" = V.black `on` rgbColor 13 168 10
+getAttr "bgLightGreen" = flip V.withStyle V.bold $ V.black `on` rgbColor 55 209 52
+getAttr "key" = flip V.withStyle V.bold $ V.black `on` rgbColor 222 221 153
+
 -- | Map of Attributes to be used when rendering
 theMap::A.AttrMap
-theMap = A.attrMap V.defAttr 
-     [("edit", V.black `on` bColor)
-     ,("status", flip V.withStyle V.bold $ V.white `on` V.black)
-     ,("command",V.white `on` V.blue)
-     ,("focussed",V.black `on` bColor)]
-  where
-    bColor = V.rgbColor 240 240 240
-
-
-makeColorCellImg :: Char -> Image
-makeColorCellImg c | c == ' ' = charFill bgColorLight ' ' 2 1
-                   | otherwise = charFill bgColorDark ' ' 2 1
-  where 
-    bgColorDark = V.black `on` blue
-    bgColorLight = V.black `on` V.rgbColor 55 209 52 
-
-gameOverStr =
-  ["                            "
-  ,"  mmmm                      "
-  ," m       mmm   mmmmm   mmm  "
-  ," #  mmm #   #  # # #  #   # "
-  ," #  m # m   #  # # #  #     "
-  ,"  #mmm   mmm#  # # #   #mm  "
-  ,"                            "
-  ,"                            "
-  ,"                            "
-  ,"  mmmm                      "
-  ," m    m m   m   mmm    mmmm "
-  ," #    #  m m   #   #   #    "
-  ," #    #  # #   #       #    "
-  ,"  #mm#    #     #mm    #    "
-  ,"                            "
-  ,"                            "
-  ]
-
-initGameStr =
-   ["#      Press Enter To Play       # "]
+theMap = A.attrMap V.defAttr []
